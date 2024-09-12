f8e2d82070c46d94331b4e1b5fe26d53c3abddd2
xinyu hu <zhangbajin@aliyun.com>
2024-09-12 20:07:26 +0000

    c

--- models/jaffle_shop/stg_orders.sql
+++ models/jaffle_shop/stg_orders.sql
@@ -1,6 +1,6 @@
 select
     id as order_id,
     customer as customer_id,
-    ORDERED_AT order_date
-
+    ORDERED_AT order_date,
+    case when order_total>1000 then order_total*-1 else order_total end as order_total
 from {{source ('jaffle_shop','orders')}}
